dc28362a594d4e66a3816161c188bfb301e1bb47
Reinhardt Rijna <reinhardtrijna@gmail.com>
2020-12-23 23:14:16 +0100

    changed server path

--- client/src/server.js
+++ client/src/server.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 import { createContext } from "react";
 
-export const socket = io.connect("http://localhost:4000", {
+export const socket = io.connect("https://reinhardtr.herokuapp.com", {
   transports: ["websocket"],
 });
 
